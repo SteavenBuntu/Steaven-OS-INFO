ad26d9aae5dfd7a1f0c54b68aabd4a313f669ebb
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-03-24 00:59:01 +0200

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo rm -rf /etc/lsb-release
+sudo rm -rf /etc/os-release
+sudo wget -O /etc/lsb-release https://github.com/SteavenBuntu/Steaven-OS-INFO/raw/main/lsb-release
+sudo wget -O /etc/os-release https://github.com/SteavenBuntu/Steaven-OS-INFO/raw/main/os-release
